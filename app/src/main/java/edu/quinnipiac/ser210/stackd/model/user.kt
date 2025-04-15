9ee1f014a595e2c079d92740205e2c90c4c27b89
R-Seel02 <Ryanjseely@icloud.com>
2025-04-15 00:17:29 -0400

    data stuff

--- app/src/main/java/edu/quinnipiac/ser210/stackd/model/user.kt
+++ app/src/main/java/edu/quinnipiac/ser210/stackd/model/user.kt
@@ -1,5 +1,18 @@
 package edu.quinnipiac.ser210.stackd.model
 
 data class User(
+    val id: Int,
+    val name: String,
+    val exercises: ArrayList<String>,
+    val targetList: ArrayList<String>,
+    val type: ArrayList<String>,
+    val bodyPart: ArrayList<String>,
+    val target: ArrayList<String>
+
+
+
+
+
+
 
 )
--- app/src/main/java/edu/quinnipiac/ser210/stackd/network/ApiService.kt
+++ app/src/main/java/edu/quinnipiac/ser210/stackd/network/ApiService.kt
@@ -1,7 +1,8 @@
 package edu.quinnipiac.ser210.stackd.network
 
-import edu.quinnipiac.ser210.model.user
-
+import edu.quinnipiac.ser210.stackd.model.User
+import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 interface ApiService {
