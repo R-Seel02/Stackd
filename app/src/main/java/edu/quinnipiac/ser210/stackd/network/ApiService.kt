56e16be9af171e5198e33a8ac43948097fb53263
BranMcCrave <brandonbryan0120@outlook.com>
2025-04-15 00:22:22 -0400

    APIservice

--- app/src/main/java/edu/quinnipiac/ser210/stackd/network/ApiService.kt
+++ app/src/main/java/edu/quinnipiac/ser210/stackd/network/ApiService.kt
@@ -1,10 +1,38 @@
 package edu.quinnipiac.ser210.stackd.network
 
-import edu.quinnipiac.ser210.model.user
-
+import edu.quinnipiac.ser210.stackd.model.User
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
 
 
 interface ApiService {
+
+//    @Headers(
+//    "7f584a90a1mshf02cb77037613e9p17a9e2jsnf0b196a8e07e"
+//    )
+
+
     @GET("users/{id}")
     suspend fun getUser(@Path("id") id: Int): User
+
+    companion object {
+
+        var BASE_URL = "https://rapidapi.com/justin-WFnsXH_t6/api/exercisedb/playground/apiendpoint_8ae18d70-75aa-4164-b4ef-0afde01402c2"
+
+
+        fun create() : ApiService{
+
+
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+
+    }
 }
