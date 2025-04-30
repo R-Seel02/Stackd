e9a45effc47f3064cd77a68182982a47fb46bb07
R-Seel02 <Ryanjseely@icloud.com>
2025-04-29 23:48:49 -0400

    main screen stuff

--- app/src/main/java/edu/quinnipiac/ser210/stackd/Screen/MainScreen.kt
+++ app/src/main/java/edu/quinnipiac/ser210/stackd/Screen/MainScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -12,6 +14,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -31,10 +34,16 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import edu.quinnipiac.ser210.stackd.Navigation.AppScreens
 import edu.quinnipiac.ser210.stackd.R
+import edu.quinnipiac.ser210.stackd.api.Exercise
+import edu.quinnipiac.ser210.stackd.api.randomExercies
 import edu.quinnipiac.ser210.stackd.model.ThemeViewModel
 import edu.quinnipiac.ser210.stackd.model.stackdViewModel
-
-
+import java.time.LocalDateTime
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import java.time.format.DateTimeFormatter
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -47,6 +56,7 @@ fun MainScreen(
     themeViewModel: ThemeViewModel
 
 ) {
+
 
   Box(
     modifier = Modifier
@@ -127,15 +137,71 @@ fun MainScreen(
     ) { innerPadding ->
       Column(modifier = Modifier.padding(innerPadding)) {
         Text(
-          text = "Welcome to the Main Screen!",
+          text = "Welcome to Stackd <Name>",
           color = Color.Black,
           fontFamily = FontFamily.SansSerif,
           fontSize = 30.sp,
-          modifier = modifier.offset(x = 20.dp,y=20.dp)
-
+          modifier = modifier
+            .align(Alignment.CenterHorizontally)
+            .padding(20.dp),
         )
+
+//        val dateSt = "2025-04-29T21:30:40Z"
+//        val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+//        val formattedDate = LocalDateTime.parse(dateSt, dateFormatter)
+//        val res = DateTimeFormatter.ofPattern("MMMM dd, yyyy | hh:mma").format(formattedDate) // August 04, 2017 | 6:39pm
+
+
+
+
+        Text(
+          text =  "Spotlight on these \n \nWorkouts :)",
+          fontSize = 30.sp,
+          fontStyle = FontStyle.Normal,
+          fontFamily = FontFamily.SansSerif,
+          modifier = Modifier.
+          align(Alignment.CenterHorizontally)
+            .padding(70.dp)
+        )
+
+
+
+
+        LaunchedEffect(Unit) {
+          stackdViewModel.getRandomExercises()
+          }
+
+        val response = stackdViewModel.exerciseResult.observeAsState()
+        val exerciseList = response.value?.body() ?: emptyList()
+
+        val exercises = stackdViewModel.exerciseResult.value?.body() ?: emptyList()
+
+
+
+        Text(
+          text = "WTW 1: ",
+          color = Color.Black,
+          fontSize = 20.sp,
+          fontStyle = FontStyle.Normal,
+          fontFamily = FontFamily.Serif,
+          modifier = Modifier.padding(10.dp)
+            .offset(x = 30.dp, y = -70.dp)
+        )
+
+
+        exercises.take(3).forEach { exercise ->
+          Text(text = exercise.name ?: "Unknown", fontSize = 18.sp)
+          Text(text = "Target: ${exercise.target ?: "?"}")
+          Text(text = "Equipment: ${exercise.equipment ?: "?"}")
+          Spacer(modifier = Modifier.height(12.dp).offset(x=90.dp,y=-35.dp))
+
+        }
+
+
+
+        }
 
       }
     }
   }
-}
+
